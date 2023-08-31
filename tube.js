0afee10dc68dbfd2707c74afccfb4599ed1a0482
biru60 <u1904060@student.cuet.ac.bd>
2023-09-01 05:17:45 +0600

    button added dynamically

--- tube.js
+++ tube.js
@@ -1,1 +1,22 @@
 const showBlog =()=>window.location.href=('blog.html');
+
+
+
+
+const showAllCatagory =async()=>{
+const res =await fetch("https://openapi.programming-hero.com/api/videos/categories");
+const data = await res.json();
+createButton(data.data);
+}
+
+const createButton =(catagoryLIst)=>{
+    const addBUtton = document.getElementById("add-button");
+    catagoryLIst.forEach(item=> {
+       const button = document.createElement("button");
+   
+    button.classList=`btn mx-2 bg-slate-300`
+       button.innerText=`${item.category}`
+       addBUtton.appendChild(button);
+    });
+}
+showAllCatagory();
