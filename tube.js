9898882e054675fbb07653219a6394b199224905
biru60 <u1904060@student.cuet.ac.bd>
2023-09-01 13:04:29 +0600

    sorting done

--- tube.js
+++ tube.js
@@ -3,6 +3,7 @@ const showBlog = () => window.location.href = ('blog.html');
 
 
 
+
 const showAllCatagory = async () => {
     const res = await fetch("https://openapi.programming-hero.com/api/videos/categories");
     const data = await res.json();
@@ -18,23 +19,33 @@ const createButton = (catagoryLIst) => {
         button.innerText = `${item.category}`
         addBUtton.appendChild(button);
         button.addEventListener("click", function () {
-            buttonClicked(item.category_id);
+            buttonClicked(item.category_id  );
+         
+            
         })
     });
 }
 
 
 const  buttonClicked = async(category_id) => {
-    
+   
     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${category_id}`)
     const cardDetails=await res.json();
     const cardDetail =cardDetails.data;
-    console.log(cardDetail);
+   
     const addCard = document.getElementById("add-card");
     addCard.textContent="";
-    if(cardDetail.length>0){
-        addCard.classList=`grid gap-4 md:grid-cols-2 lg:grid-cols-4 `
-    cardDetail.forEach(catagory => {
+   
+    document.getElementById("sort").addEventListener("click",function(){
+        addCard.textContent="";
+        const cardDetail1=[...cardDetail];
+       cardDetail1.sort((a,b)=>{
+         const view1= parseInt(a.others.views.replace('K',''));
+         const view2= parseInt(b.others.views.replace('K',''));
+         return view2-view1;
+ });
+    addCard.classList=`grid gap-4 md:grid-cols-2 lg:grid-cols-4 `
+    cardDetail1.forEach(catagory => {
         const div = document.createElement("div");
         
       
@@ -57,6 +68,37 @@ const  buttonClicked = async(category_id) => {
       
      
      addCard.appendChild(div);
+     
+    });
+       
+    
+    })
+   
+    if(cardDetail.length>0){
+        addCard.classList=`grid gap-4 md:grid-cols-2 lg:grid-cols-4 `
+    cardDetail.forEach(catagory => {
+        const div = document.createElement("div");
+        
+      
+        div.classList = `card  bg-base-100 shadow-xl p-4 `;
+      
+        div.innerHTML=`
+        <figure><img class="h-[300px]" src=${catagory.thumbnail} alt="Shoes" /></figure>
+     <div class="card-body">
+     <div class="flex gap-2 "> <img class="w-10 h-10 rounded-full " src=${catagory.authors[0].profile_picture} alt="">
+     <h2 class="card-title">${catagory.title}</h2>
+     </div>
+       <div class="">
+       <p>${catagory.authors[0].profile_name} ${ }
+       
+       <p>${catagory.others.views} Views</p>
+       </div>
+       
+     </div>
+        `
+       
+     
+     addCard.appendChild(div);
     });
 }
 else{
@@ -71,7 +113,7 @@ else{
 
 
    }
-    
+  
 }
 buttonClicked("1000");
 
